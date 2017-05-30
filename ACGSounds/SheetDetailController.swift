8e3c08a9ce3bfcafd95b865f0e4a8533ced62b3a
jcpwfloi <jcpwfloi@gmail.com>
2017-05-30 21:35:40 +0800

    Comment Section Implemented & working

--- ACGSounds/SheetDetailController.swift
+++ ACGSounds/SheetDetailController.swift
@@ -8,76 +8,39 @@
 
 import UIKit
 import Material
-import PDFReader
 
 class SheetDetailController: UIViewController {
+    internal var tableView: SheetDetailTableView!
+    
     fileprivate var backButton: IconButton!
-    fileprivate var starButton: IconButton!
     fileprivate var searchButton: IconButton!
-    
-    fileprivate var detailCard: Card!
-    fileprivate var cardToolbar: Toolbar!
-    fileprivate var cardMoreButton: IconButton!
-    
-    fileprivate var sheetIntro: UILabel!
-    fileprivate var bottomBar: Bar!
-    fileprivate var pdfButton: UIButton!
-    
-    fileprivate var detailedSheet: SingleSheet!
-    
-    //fileprivate var fabButton: FABButton!
     
     open override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Color.grey.lighten5
         
-        prepareBackButton()
-        prepareStarButton()
-        prepareSearchButton()
         prepareNavigationItem()
-        prepareCard()
         
-        flush()
+        tableView = SheetDetailTableView()
+        view.layout(tableView).edges()
     }
     
-    open override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        flush()
+        navigationItem.title = sheetDetail?.sheetName
+        navigationItem.detail = sheetDetail?.author
+        
+        getCommentsBySheetIdAndPage(sheetDetail!._id, commentPage: 1) { comments in
+            self.tableView.data = comments
+        }
     }
 }
 
 extension SheetDetailController {
-    fileprivate func flush() {
-        let detailSheetId = detailedSheet?._id ?? ""
-        
-        if detailSheetId != sheetDetail?._id {
-            navigationItem.title = sheetDetail?.sheetName
-            navigationItem.detail = sheetDetail?.author
-        
-            cardToolbar.title = sheetDetail?.sheetName
-            cardToolbar.detail = sheetDetail?.author
-        
-            sheetIntro.text = "Loading Sheet Introduction...."
-        
-            getSheetById(sheetDetail!._id) { detailedSheet in
-                self.detailedSheet = detailedSheet
-                self.sheetIntro.text = detailedSheet.description
-                self.detailCard.setNeedsLayout()
-                self.detailCard.layoutIfNeeded()
-                self.detailCard.x = 0
-                self.detailCard.y = 0
-            }
-        }
-    }
-    
     fileprivate func prepareBackButton() {
         backButton = IconButton(image: Icon.cm.arrowBack)
         backButton.addTarget(self, action: #selector(handleBackButton), for: .touchUpInside)
-    }
-    
-    fileprivate func prepareStarButton() {
-        starButton = IconButton(image: Icon.cm.star)
     }
     
     fileprivate func prepareSearchButton() {
@@ -86,66 +49,11 @@ extension SheetDetailController {
     }
     
     fileprivate func prepareNavigationItem() {
+        prepareBackButton()
+        prepareSearchButton()
+        
         navigationItem.leftViews = [backButton]
-        navigationItem.rightViews = [starButton, searchButton]
-    }
-}
-
-//prepare Cards
-
-extension SheetDetailController {
-    fileprivate func prepareCardMoreButton() {
-        cardMoreButton = IconButton(image: Icon.cm.moreVertical, tintColor: Color.grey.base)
-    }
-    
-    fileprivate func prepareCardToolbar() {
-        prepareCardMoreButton()
-        
-        cardToolbar = Toolbar(rightViews: [cardMoreButton])
-        
-        cardToolbar.titleLabel.textAlignment = .left
-        cardToolbar.detailLabel.textAlignment = .left
-        cardToolbar.detailLabel.textColor = Color.grey.base
-    }
-    
-    fileprivate func prepareSheetIntro() {
-        sheetIntro = UILabel()
-        sheetIntro.numberOfLines = 0
-        sheetIntro.text = "Loading Sheet Introduction...."
-        sheetIntro.font = RobotoFont.regular(with: 14)
-    }
-    
-    fileprivate func prepareCard() {
-        detailCard = Card()
-        
-        prepareCardToolbar()
-        prepareSheetIntro()
-        prepareBottomBar()
-        
-        detailCard.toolbar = cardToolbar
-        detailCard.toolbarEdgeInsetsPreset = .square3
-        detailCard.toolbarEdgeInsets.bottom = 0
-        detailCard.toolbarEdgeInsets.right = 8
-        
-        detailCard.contentView = sheetIntro
-        detailCard.contentViewEdgeInsetsPreset = .wideRectangle3
-        
-        detailCard.bottomBar = bottomBar
-        detailCard.bottomBarEdgeInsetsPreset = .wideRectangle2
-        
-        view.layout(detailCard).top(0).left(0).width(view.width)
-    }
-    
-    fileprivate func prepareBottomBar() {
-        bottomBar = Bar()
-        
-        pdfButton = UIButton()
-        pdfButton.setTitle("Open PDF", for: .normal)
-        pdfButton.setTitleColor(Color.blue.base, for: .normal)
-        pdfButton.titleLabel?.font = RobotoFont.regular(with: 12)
-        pdfButton.addTarget(self, action: #selector(handlePDFButton), for: .touchUpInside)
-        
-        bottomBar.leftViews = [pdfButton]
+        navigationItem.rightViews = [searchButton]
     }
 }
 
@@ -157,21 +65,8 @@ extension SheetDetailController {
     }
     
     @objc
-    fileprivate func handleNextButton() {
-    }
-    
-    @objc
     fileprivate func handleSearchButton() {
         let window = view.window!
         window.rootViewController = searchView
-    }
-    
-    @objc
-    fileprivate func handlePDFButton() {
-        let remotePDFDocumentURLPath = detailedSheet.pdfUrl
-        let remotePDFDocumentURL = URL(string: remotePDFDocumentURLPath)!
-        let document = PDFDocument(url: remotePDFDocumentURL)!
-        let readerController = PDFViewController.createNew(with: document)
-        navigationController?.pushViewController(readerController, animated: true)
     }
 }
---
+++ ACGSounds/SheetDetailTableView.swift
@@ -0,0 +1,90 @@
+//
+//  SheetDetailTableView.swift
+//  ACGSounds
+//
+//  Created by David Chen on 30/05/2017.
+//  Copyright © 2017 FropsVM Networks Limited. All rights reserved.
+//
+
+import UIKit
+import Material
+
+class SheetDetailTableView: UITableView {
+    internal lazy var heights = [IndexPath: CGFloat]()
+    
+    public var data = [Comment]() {
+        didSet {
+            reloadData()
+        }
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        prepare()
+    }
+    
+    public init() {
+        super.init(frame: .zero, style: .plain)
+        prepare()
+    }
+    
+    /// Prepares the tableView.
+    open func prepare() {
+        delegate = self
+        dataSource = self
+        separatorStyle = .none
+        backgroundColor = nil
+        
+        register(CardTableViewCell.self, forCellReuseIdentifier: "CardTableViewCell")
+        register(CommentCell.self, forCellReuseIdentifier: "CommentCell")
+    }
+}
+
+extension SheetDetailTableView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return data.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return nil
+        } else {
+            return "Comments"
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    /// Prepares the cells within the tableView.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CardTableViewCell", for: indexPath) as! CardTableViewCell
+            cell.data = sheetDetail
+            cell.isLast = true
+            heights[indexPath] = cell.height
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath) as! CommentCell
+            cell.numberIndicated = indexPath.row + 1
+            cell.data = data[indexPath.row]
+            cell.isLast = indexPath.row == data.count - 1
+            heights[indexPath] = cell.height
+            return cell
+        }
+    }
+}
+
+extension SheetDetailTableView: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return heights[indexPath] ?? height
+    }
+}
